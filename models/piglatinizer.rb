75edd187aac6b89f8fdb481f5a694266bd92ee76
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-23 18:56:13 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -20,34 +20,34 @@ class PigLatinizer
 
   def piglatinize(word)
 
-      # if word.length == 1
-      #   new_word = word + "way"
-      # elsif @@vowels.include?(word[0]) && word.length > 1
-      #   new_word = word + "way"
-      # end
-      new_word = ""
-    word.each_char.with_index(0) do |letter, i|
-      if @@vowels.include?(letter) && word.length > 1
-        new_word = word[i..-1] + word[0...i-1] + "ay"
-      else
-        new_word = word + "way"
-      end
-    end
-    new_word
-  end
-  #
-  #   if word.length == 1
-  #     new_word = word + "way"
-  #   elsif @@vowels.include?(word[0]) && word.length > 1
-  #     new_word = word + "way"
-  #   elsif @@vowels.include?(word[1])
-  #     new_word = word[1..-1] + word[0] + "ay"
-  #   elsif @@vowels.include?(word[2])
-  #     new_word = word[2..-1] + word[0..1] + "ay"
-  #   elsif @@vowels.include?(word[3])
-  #     new_word = word[3..-1] + word[0..2] + "ay"
+  #     # if word.length == 1
+  #     #   new_word = word + "way"
+  #     # elsif @@vowels.include?(word[0]) && word.length > 1
+  #     #   new_word = word + "way"
+  #     # end
+  #     new_word = ""
+  #   word.each_char.with_index(0) do |letter, i|
+  #     if @@vowels.include?(letter) && word.length > 1
+  #       new_word = word[i..-1] + word[0...i-1] + "ay"
+  #     else
+  #       new_word = word + "way"
+  #     end
   #   end
   #   new_word
   # end
+  #
+    if word.length == 1
+      new_word = word + "way"
+    elsif @@vowels.include?(word[0]) && word.length > 1
+      new_word = word + "way"
+    elsif @@vowels.include?(word[1])
+      new_word = word[1..-1] + word[0] + "ay"
+    elsif @@vowels.include?(word[2])
+      new_word = word[2..-1] + word[0..1] + "ay"
+    elsif @@vowels.include?(word[3])
+      new_word = word[3..-1] + word[0..2] + "ay"
+    end
+    new_word
+  end
 
 end
