5ba92f1c23d5f80d32eb0caab741a25e203ef7ae
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-23 18:44:33 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/' do
+    @piglatin_string = PigLatinizer.new(params[:user_phrase])
+
+    erb :results 
+  end
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,36 @@
+class PigLatinizer
+
+  attr_reader :text
+
+  @@vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+
+  def initialize
+    @text = text
+  end
+
+  def piglatinize_sentence(sentence)
+
+    sentence.split(" ").each do |word|
+      puts piglatinize(word)
+    end
+    
+  end
+
+
+
+  def piglatinize(word)
+    if word.length == 1
+      new_word = word + "way"
+    elsif @@vowels.include?(word[0]) && word.length > 1
+      new_word = word + "way"
+    elsif @@vowels.include?(word[1])
+      new_word = word[1..-1] + word[0] + "ay"
+    elsif @@vowels.include?(word[2])
+      new_word = word[2..-1] + word[0..1] + "ay"
+    elsif @@vowels.include?(word[3])
+      new_word = word[3..-1] + word[0..2] + "ay"
+    end
+    new_word
+  end
+
+end
